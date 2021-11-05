e57f17875a9c05e47e95965f989d077da7bf0509
ohad marmor <ohad@elmar.co.il>
2021-11-05 05:55:41 -0700

    ohad TimeSeries

--- TimeSeries.cpp
+++ TimeSeries.cpp
@@ -1,8 +1,37 @@
 #include "TimeSeries.h"
-class TimeSeries {
-public:
-    TimeSeries(const char* CSVfileName){
 
+TimeSeries::TimeSeries(const char* CSVfileName) : csvFname(CSVfileName){
+    // open the file and check if valid:
+    ifstream myFile(csvFname);
+    if(!myFile.is_open()) throw std::runtime_error("Could not open file");
+    string stringHeadLine;
+    // get the first line of the file (the headlines):
+    getline(myFile, stringHeadLine);
+    // convert the string to stream:
+    istringstream streamHeadline(stringHeadLine);
+    string item;
+    // insert each headline to the corresponding column:
+    while (getline(streamHeadline, item, ',')) {
+        headLines.push_back(item);
     }
-
-};
+    unsigned int num_columns = headLines.size();
+    string line;
+    // create the specific number of columns:
+    for (int i = 0; i < num_columns; i ++) {
+        columns.emplace_back();
+    }
+    // read each line in the file:
+    while (std::getline(myFile, line)) {
+        istringstream streamLine(line);
+        // check the line is valid:
+        if(count(line.begin(), line.end(), ',') != num_columns - 1) {
+            throw runtime_error("table is invalid");
+        }
+        // insert value to each column:
+        for (int i = 0; i < num_columns; i++) {
+            getline(streamLine, item, ',');
+            float num = stof(item);
+            columns[i].push_back(num);
+        }
+    }
+}
--- TimeSeries.h
+++ TimeSeries.h
@@ -2,16 +2,24 @@
 
 #ifndef TIMESERIES_H_
 #define TIMESERIES_H_
-
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
 using namespace std;
 
 class TimeSeries{
-
+private:
+    string csvFname;
 public:
 
-    TimeSeries(const char* CSVfileName){
-    }
+    TimeSeries(const char* CSVfileName);
 
+// the array with headlines of each column:
+vector<string> headLines;
+    vector<vector<float>> columns;
 };
 
 
